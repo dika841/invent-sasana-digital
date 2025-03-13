58bd83ef5c32c9dc629e1cd3be04db1998a43c69
dika841 <randikaa1999@gmail.com>
2025-03-14 01:57:22 +0700

    Fix : Update sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,15 @@ import Image from "next/image";
 import HeaderMenu from "../_components/ui/header-menu";
 import UserProfile from "../_components/ui/user-profile";
 import { PropsWithChildren } from "react";
+import {
+  BadgePercent,
+  Box,
+  ChartColumn,
+  FileText,
+  LayoutDashboard,
+  Package,
+  UsersRound,
+} from "lucide-react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
@@ -14,8 +23,8 @@ const Layout = ({ children }: PropsWithChildren) => {
             <Image
               src="/assets/brand-logo.svg"
               alt="Sasana Digital Logo"
-              height={64}
-              width={125}
+              height={77}
+              width={181}
               priority
             />
           </Flex>
@@ -28,25 +37,112 @@ const Layout = ({ children }: PropsWithChildren) => {
         ),
       }}
       sidebar={{
-        width: 250,
+        width: 300,
         menu: [
           {
             label: "Dashboard",
-            key: "/dashboard",
+            key: "dashboard",
+            icon: <LayoutDashboard />,
           },
           {
-            key: "sub1",
-            label: "Navigation Two",
-
+            key: "report",
+            label: "Laporan",
+            icon: <FileText />,
             children: [
-              { key: "3", label: "Option 3" },
-              { key: "4", label: "Option 4" },
+              { key: "report-1", label: "Option 1" },
+              { key: "report-2", label: "Option 2" },
               {
-                key: "sub1-2",
+                key: "report-sub-1",
                 label: "Submenu",
                 children: [
-                  { key: "5", label: "Option 5" },
-                  { key: "6", label: "Option 6" },
+                  { key: "report-sub-1-1", label: "Option 3" },
+                  { key: "report-sub-1-2", label: "Option 4" },
+                ],
+              },
+            ],
+          },
+          {
+            key: "analytics",
+            label: "Analisis Laporan",
+            icon: <ChartColumn />,
+            children: [
+              { key: "analytics-1", label: "Option 1" },
+              { key: "analytics-2", label: "Option 2" },
+              {
+                key: "analytics-sub-1",
+                label: "Submenu",
+                children: [
+                  { key: "analytics-sub-1-1", label: "Option 3" },
+                  { key: "analytics-sub-1-2", label: "Option 4" },
+                ],
+              },
+            ],
+          },
+          {
+            key: "product",
+            label: "Produk",
+            icon: <Box />,
+            children: [
+              { key: "product-1", label: "Option 1" },
+              { key: "product-2", label: "Option 2" },
+              {
+                key: "product-sub-1",
+                label: "Submenu",
+                children: [
+                  { key: "product-sub-1-1", label: "Option 3" },
+                  { key: "product-sub-1-2", label: "Option 4" },
+                ],
+              },
+            ],
+          },
+          {
+            key: "inventory",
+            label: "Inventori",
+            icon: <Package />,
+            children: [
+              { key: "inventory-1", label: "Pembelian Stok" },
+              {
+                key: "inventory-sub-1",
+                label: "Kelola Stok",
+                children: [
+                  { key: "inventory-sub-1-1", label: "Daftar Stok" },
+                  { key: "inventory-sub-1-2", label: "Stok Opname" },
+                  { key: "inventory-sub-1-3", label: "Stok Terbuang" },
+                ],
+              },
+              { key: "inventory-2", label: "Daftar Pemasok (Supplier)" },
+            ],
+          },
+          {
+            key: "customer",
+            label: "Pelanggan",
+            icon: <UsersRound />,
+            children: [
+              { key: "customer-1", label: "Option 1" },
+              { key: "customer-2", label: "Option 2" },
+              {
+                key: "customer-sub-1",
+                label: "Submenu",
+                children: [
+                  { key: "customer-sub-1-1", label: "Option 3" },
+                  { key: "customer-sub-1-2", label: "Option 4" },
+                ],
+              },
+            ],
+          },
+          {
+            key: "promotion",
+            label: "Promosi",
+            icon: <BadgePercent />,
+            children: [
+              { key: "promotion-1", label: "Option 1" },
+              { key: "promotion-2", label: "Option 2" },
+              {
+                key: "promotion-sub-1",
+                label: "Submenu",
+                children: [
+                  { key: "promotion-sub-1-1", label: "Option 3" },
+                  { key: "promotion-sub-1-2", label: "Option 4" },
                 ],
               },
             ],
